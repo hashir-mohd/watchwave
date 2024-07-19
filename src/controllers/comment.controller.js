13d5b4aec070506ad8020e1330bd5f7fea3ad1e8
one <esport.py.m45@gmail.com>
2024-07-19 10:30:13 +0530

    completed comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,9 +5,104 @@ import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
+    const isGuest = req.query.guest === "true";
+    
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400,"Invalid video id");
+    }
+    const video = await Video.findById(videoId);
+    
+    if(!video){
+        throw new ApiError(404,"Video does not exist");
+    }
+
+    const commentsAggregate = Comment.aggregate([
+        {
+            $match:{
+                video : new mongoose.Types.ObjectId(videoId),
+            },
+        },
+        {
+            $lookup:{
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            },
+        },
+        {
+            $lookup:{
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes"
+            },
+        },
+        {
+            $addFields:{
+                likesCount: {
+                    $size: "$likes"
+                },
+                owner:{
+                    $first: "$owner"
+                },
+                isLiked:{
+                    $cond:{
+                        if:isGuest,
+                        then:false,
+                        else :{
+                            $cond:{
+                                if:{$in:[req.user?._id,"$likes.likedBy"]},
+                                then:true,
+                                else:false
+                            }
+                        }
+                    }
+                },
+                
+            }
+        },
+        {
+            $sort:{
+                createdAt: -1
+            }
+        },
+        {
+            $project:{
+                content: 1,
+                createdAt: 1,
+                likesCount: 1,
+                owner:{
+                    username: 1,
+                    fullName: 1,
+                    "avatar.url": 1,
+                    _id: 1,
+                },
+                isLiked: 1
+            }
+        }
+    ])
+    
+    if(!commentsAggregate){
+        throw new ApiError(500, "Something went wrong while fetching comments");
+    }
+    const options ={
+        page: parseInt(page,10),
+        limit: parseInt(limit,10),
+    }
+    const comments = await Comment.aggregatePaginate(commentsAggregate,options);
+    if(!comments){
+        throw new ApiError(500, "Something went wrong while paginating comments");
+    }
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, comments, "Comments fetched successfully")
+    )
+
+
 
 })
 
@@ -115,5 +210,5 @@ export {
     getVideoComments, 
     addComment, 
     updateComment,
-     deleteComment
+    deleteComment
     }

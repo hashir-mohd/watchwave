2a2d6a050fd22706e23141624460905426d837d7
one <esport.py.m45@gmail.com>
2024-10-28 20:38:15 +0530

    test

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -429,26 +429,27 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-    {import.meta.env.MODE === "development" && (
-      <ReactQueryDevtools initialIsOpen={false} />
-    )}
-    <Toaster
-      position="bottom-right"
-      reverseOrder={true}
-      toastOptions={{
-        error: {
-          style: { borderRadius: "0", color: "red" },
-        },
-        success: {
-          style: { borderRadius: "0", color: "green" },
-        },
-        duration: 2000,
-      }}
-    />
-  </QueryClientProvider>
+  // <QueryClientProvider client={queryClient}>
+  //   <Provider store={store}>
+  //     <RouterProvider router={router} />
+  //   </Provider>
+  //   {import.meta.env.MODE === "development" && (
+  //     <ReactQueryDevtools initialIsOpen={false} />
+  //   )}
+  //   <Toaster
+  //     position="bottom-right"
+  //     reverseOrder={true}
+  //     toastOptions={{
+  //       error: {
+  //         style: { borderRadius: "0", color: "red" },
+  //       },
+  //       success: {
+  //         style: { borderRadius: "0", color: "green" },
+  //       },
+  //       duration: 2000,
+  //     }}
+  //   />
+  // </QueryClientProvider>
+  <h1>hello</h1>
   // </React.StrictMode>
 );

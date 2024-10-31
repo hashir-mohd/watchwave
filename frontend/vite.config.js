84cebcda958d6fb6ff837deae7af530eb938f426
one <esport.py.m45@gmail.com>
2024-10-31 19:09:00 +0530

    added prod build cmds

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode === "production" ? "prod" : "", process.cwd());
+
+  return {
+    plugins: [react()],
+    define: {
+      "process.env": env,
+    },
+    // Example: set base path or other config options using env variables
+    base: env.VITE_BASE_URL || "/",
+  };
+});

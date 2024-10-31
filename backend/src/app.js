0fa1081d4fa0996c0d160c1616fcddb6eca7f507
one <esport.py.m45@gmail.com>
2024-10-31 14:10:14 +0530

    env deleted

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,25 +4,17 @@ import cors from "cors";
 
 const app = express();
 
-const allowedOrigins = [
-  process.env.CORS_ORIGIN_DEV, 
-  process.env.CORS_ORIGIN_PRODUCTION,
-];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (like mobile apps or Postman)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    origin:
+      process.env.MODE === "development"
+        ? process.env.CORS_ORIGIN_DEV
+        : process.env.CORS_ORIGIN_PRODUCTION,
+    credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
